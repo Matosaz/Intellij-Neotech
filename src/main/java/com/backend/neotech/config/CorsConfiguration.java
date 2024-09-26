847e0550f8eee6cefb35a3fabf2d63ede189156c
Matosaz <mathusphd20@gmail.com>
2024-09-26 09:51:44 -0300

    CRUD FUNCIONANDO

---
+++ src/main/java/com/backend/neotech/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.backend.neotech.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Permite todas as rotas que começam com /api
+                .allowedOrigins("http://localhost:5173") // Permite origem do frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos os headers
+                .allowCredentials(true); // Permite credenciais
+    }
+}
