cc49215f7bd211d06de6f55c4cc567319f082996
Matosaz <mathusphd20@gmail.com>
2025-03-15 12:09:20 -0300

    Ambiente de teste local

--- src/main/java/com/backend/neotech/config/CorsConfiguration.java
+++ src/main/java/com/backend/neotech/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Permite todas as rotas que começam com /api
-                .allowedOrigins("https://neotechgroup.netlify.app") // Permite origem do frontend
+                .allowedOrigins("https://localhost:5173") // Permite origem do frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os headers
                 .allowCredentials(true); // Permite credenciais
