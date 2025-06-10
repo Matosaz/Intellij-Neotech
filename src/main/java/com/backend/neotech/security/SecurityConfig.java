0848673b453c0b7e6b6a51a271bd15cdd68b2816
Matosaz <mathusphd20@gmail.com>
2025-06-10 12:58:29 -0300

    Atualização de permissão de rotas

--- src/main/java/com/backend/neotech/security/SecurityConfig.java
+++ src/main/java/com/backend/neotech/security/SecurityConfig.java
@@ -35,12 +35,13 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())     // desabilita CSRF (útil para API REST)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/users/login").permitAll()
-                        .requestMatchers("/api/v1/orcamentos").permitAll()
-                        .requestMatchers("/api/v1/categorias").permitAll()
-
+                        .requestMatchers(HttpMethod.GET, "/api/v1/orcamentos/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/orcamentos").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/categorias/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/users/**").permitAll()
                         .anyRequest().authenticated()
                 );
+
         // aqui você adiciona filtros de autenticação, se tiver (JWT, etc.)
 
         return http.build();
