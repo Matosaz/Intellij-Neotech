2a826a168142aef76b4600b9bc9c7801f6408afc
Matosaz <mathusphd20@gmail.com>
2025-10-01 17:01:10 -0300

    Optimization for list of data from Ranking Page

--- src/main/java/com/backend/neotech/NeotechApplication.java
+++ src/main/java/com/backend/neotech/NeotechApplication.java
@@ -2,12 +2,17 @@ package com.backend.neotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 
 public class NeotechApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load(); // carrega .env
+        System.setProperty("SPRING_USERNAME", dotenv.get("SPRING_USERNAME"));
+        System.setProperty("SPRING_PASSWORD", dotenv.get("SPRING_PASSWORD"));
+        System.setProperty("SENDGRID_API_KEY", dotenv.get("SENDGRID_API_KEY"));
         SpringApplication.run(NeotechApplication.class, args);
     }
 
