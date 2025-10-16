ad7a77a5888cdd0e4aa5394b456baa96b1049af9
Matosaz <mathusphd20@gmail.com>
2025-10-16 17:23:24 -0300

    Fixes for User CodStatus manipulation

--- src/main/java/com/backend/neotech/service/EmailService.java
+++ src/main/java/com/backend/neotech/service/EmailService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
-//teste
+//test
 @Service
 public class EmailService {
 
